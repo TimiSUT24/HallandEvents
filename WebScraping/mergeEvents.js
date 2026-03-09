ffdd59d795c971552a176eaaee352fde8f920aee
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2026-03-09 10:55:44 +0100

    fix

--- WebScraping/mergeEvents.js
+++ WebScraping/mergeEvents.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const files = fs.readdirSync('.').filter(f => f.endsWith('.json') && f !== 'allEvents.json');
+const files = fs.readdirSync('.').filter(f => f.startsWith('events') && f !== 'allEvents.json');
 
 let mergedEvents = [];
 
