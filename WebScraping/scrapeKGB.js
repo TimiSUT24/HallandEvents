48c22f1baab3200523e2a6bccebddb97785376a3
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2026-04-08 15:51:02 +0200

    kgb script almost complete just small fixes to be done

--- WebScraping/scrapeKGB.js
+++ WebScraping/scrapeKGB.js
@@ -32,9 +32,30 @@ const kgbCategories = 'https://kungsbacka.se/uppleva-och-gora/evenemang';
             console.log('⚠️ Cookie banner not found or already accepted.');
         }
 
+       function formatDate(startDateObj, endDateObj){
+            const formatTime = (timestamp) =>  {
+                if(!timestamp) return "";
+
+                return new Intl.DateTimeFormat("sv-SE",{
+                    timeZone: "Europe/Stockholm",
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    hour12: false
+                }).format(new Date(Number(timestamp)));
+            };
+
+            const start = formatTime(startDateObj?.timestamp);
+            const end = formatTime(endDateObj?.timestamp);
+
+            if (start && end) return `${start} - ${end}`;
+            if (start) return start;
+            if (end) return end;      
+            return "";   
+        }
+
         async function getCategories(page){
 
-            await page.locator(".lp-filterable-list-control-group--button", {hasText: "Kategori"}).click();
+                await page.locator(".lp-filterable-list-control-group--button", {hasText: "Kategori"}).click();
 
 
                 const responsePromise = page.waitForResponse(response =>
@@ -45,32 +66,99 @@ const kgbCategories = 'https://kungsbacka.se/uppleva-och-gora/evenemang';
                 
                 const response = await responsePromise;
                 const data = await response.json();
-                const categories = data.categories?.[0]?.options?.map(option => ({
+                const categories = data.categories?.[0]?.categories?.map(option => ({
                         value: option.value
                     })) || [];
+                await page.locator('.lp-filterable-list-control').filter({hasText: "Barn och familj"}).locator('label span').click();
 
-            return categories;
+                return categories;
 
         }
         const categories = await getCategories(page);
-        console.log(categories);
         const eventMap = new Map();
+        console.log(categories);
         
         for(const category of categories){
             console.log(`🔍 Scraping category "${category.value}"`);
 
             const categoryResponsePromise = page.waitForResponse(res =>
-                res.url().includes("svAjaxReqParam=ajax")
+                res.url().includes("svAjaxReqParam=ajax") &&
+                res.url().includes("category_419fc51e-bd32-4c73-9f34-fb26456ece43")
             );           
             await page.locator('.lp-filterable-list-control').filter({hasText: category.value}).locator('label span').click();
 
             let response = await categoryResponsePromise;
             let data = await response.json();
-            const items = data.items || [];
-            const mappedCategories = normalizeCategories(category, 'kgb');
-            console.log(data);
+
+            let items = data.items || [];
+            const mappedCategories = normalizeCategories(category.value, 'kgb');
+            
+            const event = items.map(item => ({
+                name: item?.name ?? "",
+                link: item?.link ? `https://kungsbacka.se${item.link}` : "",
+                img:  item?.image?.url ? `https://kungsbacka.se${item.image.url}` : "",
+                ort: 'Kungsbacka',
+                dates: [
+                    {
+                        startDate: item.date.startDate.screenreader ?? "",
+                        endDate: item.date.endDate.screenreader ?? "",
+                        time: formatDate(item?.date?.startDate, item?.date?.endDate)
+                    }
+                ]
+
+            }));
+            console.log(event);
+
+
+            const eventKey = (event) => `${event.name}-${event.dates[0].startDate}`;
+            mapEvents(eventMap,event,mappedCategories, eventKey);
+
+            while(true){
+                const nextButton = page.locator('.env-pagination__link--next');
+                
+                if (!(await nextButton.isVisible())) break;
+                if (!(await nextButton.isEnabled())) break;
+
+                const nextResponsePromise = page.waitForResponse(res =>
+                    res.url().includes("svAjaxReqParam=ajax") &&
+                    res.url().includes("category_419fc51e-bd32-4c73-9f34-fb26456ece43")
+                );
+
+                await nextButton.click();
+
+                response = await nextResponsePromise;
+                data = await response.json();
+
+                items = data.items || [];
+                
+                const event = items.map(item => ({
+                    name: item?.name ?? "",
+                    link: item?.link ? `https://kungsbacka.se${item.link}` : "",
+                    img:  item?.image?.url ? `https://kungsbacka.se${item.image.url}` : "",
+                    ort: 'Kungsbacka',
+                    dates: [
+                        {
+                            startDate: item.date.startDate.screenreader ?? "",
+                            endDate: item.date.endDate.screenreader ?? "",
+                            time: formatDate(item?.date?.startDate, item?.date?.endDate) 
+                        }
+                    ]
+
+                }));
+                console.log(event);
+
+                const eventKey = (event) => `${event.name}-${event.dates[0].startDate}`;
+                mapEvents(eventMap,event,mappedCategories, eventKey);
+                
+            }  
+            
+            await page.waitForTimeout(2000);
+            await page.locator('.lp-filterable-list-control').filter({hasText: category.value}).locator('label span').click();       
         }
 
-        await page.waitForTimeout(5000);
-    await browser.close();
+            const allEvents = Array.from(eventMap.values());
+            const filePath = path.join(__dirname, 'eventsKGB.json');
+            fs.writeFileSync(filePath, JSON.stringify(allEvents, null, 2), 'utf-8');
+            console.log(`📝 Saved ${allEvents.length} events to eventsKGB.json`);
+            await browser.close();
 })();
